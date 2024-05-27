e4f590ff98b670d9520087b81cb19b73cd67f0d8
AhmadMWaddah <wddh.ahmd@gmail.com>
2024-05-27 12:34:35 +0300

    Orders Initiating
    Starting the basic pages For Orders app.

--- orders/orders.py
+++ orders/orders.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class CheckOutView(TemplateView):
+    template_name = "bags/checkout.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(CheckOutView, self).get_context_data(**kwargs)
+        context["page"] = "Checkout"
+        return context
